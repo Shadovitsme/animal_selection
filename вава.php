aeeeef19bb59f0f8a53e6738fa08cbcd797545ed
ukki <iiii@mail.ru>
2024-07-16 16:15:36 +0300

    first dz work!

--- вава.php
+++ вава.php
@@ -1,1 +1,8 @@
 <?php
+
+require_once '/home/ukki/www/obuchalovo/classes/FileWriter.php';
+
+use classes\FileWriter\FileWriter;
+
+$test = new FileWriter('test.txt', 'test connection');
+$test->WriteFile('test.txt', 'test connection');
