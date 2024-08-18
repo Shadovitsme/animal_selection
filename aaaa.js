d3b486a0dff9735e373cdaf88c1454a06c487106
ukki <iu.cashirskaia@yandex.ru>
2024-08-18 13:59:04 +0300

    продолжаем с апи

--- aaaa.js
+++ aaaa.js
@@ -1,1 +1,25 @@
-$('p:contains("��������")').css('color', 'red');
+$(document).ready(function () {
+    $.ajaxSetup({
+        url: "ttps://jsonplaceholder.typicode.com/posts",
+        context: $(".content"),
+        404: function () {
+            alert("not found");
+        },
+        success: [function () {
+            $("button").text("done");
+        }],
+        error: function (xhr) {
+            console.log('1');
+            alert(`Error ${xhr.status} error text ${xhr.statusText}`);
+        },
+    })
+    $("button").click(function () {
+        $.ajax();
+
+    });
+});
+
+
+// эксперементы с автонастройкой графика
+const attr = $('td[style]');
+console.log(attr);
