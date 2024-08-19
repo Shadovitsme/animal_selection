649caa753b3b0b859b43a8c78d5f28591f9e7537
ukki <iu.cashirskaia@yandex.ru>
2024-08-19 22:46:46 +0300

    before nasled delo

--- aaaa.js
+++ aaaa.js
@@ -17,9 +17,35 @@ $(document).ready(function () {
         $.ajax();
 
     });
-});
+
 
 
 // эксперементы с автонастройкой графика
-const attr = $('td[style]');
-console.log(attr);
+const dataArr = [1,7,10,5,8,4];
+//заполнение нулевого элемента
+const del = 15;
+let nu = dataArr[0]/del;
+let sec = dataArr[1]/del;
+$("caption").after(`
+    <tbody>
+        <tr id = '0'>
+            <td style="--start: ${nu}; --size: ${sec}">
+                <span class="data"> ${dataArr[0]}  </span>
+            </td>
+        </tr>
+        </tbody>`);
+        nu = sec;
+        sec = dataArr[2]/del;
+//цикл заполнения остальных элементов
+for (var i = 1; i<dataArr.length; i++) {
+    $(`tr#${i-1}`).after(
+        `<tr id = "${i}">
+            <td style="--start: ${nu}; --size: ${sec}">
+                <span class="data"> ${dataArr[i]}  </span>
+            </td>
+        </tr>`
+    )
+    nu = sec;
+    if (i+1 <= dataArr.length) sec = dataArr[i+1]/del;
+}
+});
