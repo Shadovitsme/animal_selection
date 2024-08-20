2e05931511004f85850ed7fb64655ba8892eb8a5
ukki <iu.cashirskaia@yandex.ru>
2024-08-20 17:05:03 +0300

    подняла бд и заставила работать добавление зверья в веб форму

--- recessiva/recessiva_try.js
+++ recessiva/recessiva_try.js
@@ -1,19 +1,35 @@
 class animal{
-    constructor(name, color, spots, features){
+    constructor(name, color, spots, features, mother, father, male){
         this.name = name;
         this.color = color;
         this.spots = spots;
         this.features = features;
+        this.mother = mother;
+        this.father = father;
+        this.male = male;
     }
 };
 //добавить ссылку на мать и отца добавить расчет степени родства при нажатии на элемент
+// 
 
-let ani = new animal("ягода","red", "a lot of", "wgite tale" );
-console.log(ani);
-let i =0;
-$(`tr#${i}`).after(
-    `   <td>${ani.name}</td>
+
+
+$("button").click(function(){
+    let name = document.getElementById('name');
+    let color = document.getElementById('color');
+    let spots = document.getElementById('spots');
+    let features = document.getElementById('features');
+    let ani = new animal(name.value, color.value, spots.value, features.value);
+    console.log(name);
+    let i =0;
+    $(`tr#${i}`).after(
+        `<tr id = ${i++}>
+        <td>${ani.name}</td>
         <td>${ani.color}</td>
         <td>${ani.spots}</td>
-        <td>${ani.features}</td>`
-)
+        <td>${ani.features}</td>
+        </tr>`
+    )
+    i++;
+    return false;
+});
